634604a15bcc227bf8588aa47d2fcf4c80130aca
Nguyen Hoa Ninh Dan <dh52007049@student.stu.edu.vn>
2024-07-24 05:59:43 +0700

    fix CommentLikeGetAllByCommentID

--- services/comment_like.go
+++ services/comment_like.go
@@ -13,7 +13,7 @@ func NewCommentLikeService() *CommentLikeService {
 }
 
 func (cl *CommentLikeService) CommentLikeGetAllByCommentID(commentID string, commentLikes interface{}) error {
-	if err := common.DBConn.Model(&entity.PostLike{}).Where("comment_id = ? AND is_comment_liked = ?", commentID, true).Find(commentLikes).Error; err != nil {
+	if err := common.DBConn.Model(&entity.CommentLike{}).Where("comment_id = ? AND is_comment_liked = ?", commentID, true).Find(commentLikes).Error; err != nil {
 		return errors.New("error when getting comment likes by comment id")
 	}
 
