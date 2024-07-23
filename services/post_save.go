dd6cff70d968ae7a7d0a04d4814ffa32aa90017f
Nguyen Hoa Ninh Dan <dh52007049@student.stu.edu.vn>
2024-07-23 20:23:36 +0700

    fix post save trả về kết quả sai

--- services/post_save.go
+++ services/post_save.go
@@ -13,7 +13,7 @@ func NewPostSaveService() *PostSaveService {
 }
 
 func (pl *PostSaveService) PostSaveGetAllByPostID(postID string, postSaves interface{}) error {
-	if err := common.DBConn.Model(&entity.PostLike{}).Where("post_id = ?", postID).Find(postSaves).Error; err != nil {
+	if err := common.DBConn.Model(&entity.PostSave{}).Where("post_id = ?", postID).Find(postSaves).Error; err != nil {
 		return errors.New("error when getting post saves by post id")
 	}
 
